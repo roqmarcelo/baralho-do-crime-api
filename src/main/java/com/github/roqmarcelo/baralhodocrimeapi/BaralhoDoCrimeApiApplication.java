bf2574f38f6aa849945ca06ae563b14e3eb5f4d3
roqmarcelo <roque.santoro@gmail.com>
2019-09-21 20:07:38 -0300

    primeiro commit

---
+++ src/main/java/com/github/roqmarcelo/baralhodocrimeapi/BaralhoDoCrimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.roqmarcelo.baralhodocrimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaralhoDoCrimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaralhoDoCrimeApiApplication.class, args);
+	}
+
+}
